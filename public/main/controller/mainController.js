c823eebde84c5e8b4a7dae7146e82d5e2de5a21a
Brandon McNary <bmcnary@live.com>
2015-01-13 16:11:29 -0800

    removed angular refs

--- public/main/controller/mainController.js
+++
@@ -1,5 +0,0 @@
-// mostly the user login function, which could be split out into factories
-// other functions can be split out
-angular.module('main').controller('mainController', ['$scope', function($scope){
-console.log('within main controller')
-}]);
